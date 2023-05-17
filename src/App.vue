1fc2c4c99c0f3f9f7251be083ab8961867c6413f
dashdashzako <d@dashdashzako.net>
2023-05-17 10:06:19 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { createIntl } from '@formatjs/intl'
+import { provideIntl } from 'vue-intl';
+import HelloWorld from './components/HelloWorld.vue';
+
+provideIntl(
+  createIntl({
+    locale: 'en',
+    defaultLocale: 'en',
+    messages: {
+      foo: 'Composed',
+    },
+  })
+)
+</script>
+
+<template>
+  🎁
+  <HelloWorld />
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useIntl } from 'vue-intl'
+
+const intl = useIntl()
+</script>
+
+<template>
+  {{ intl.formatMessage({
+    id: 'Hello World',
+    defaultMessage: 'This is hello world',
+    description: {
+      whatever: 1,
+      tags: 'rainbow, kittens',
+      otherMeta: 'why not'
+    }
+  }) }}
+</template>
